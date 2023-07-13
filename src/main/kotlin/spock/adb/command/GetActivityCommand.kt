93d23e6fc29fd48f37131f7147fa99840678740a
史文斌 <shiwenbin@haizitong.com>
2023-07-13 11:32:22 +0800

    支持android13获取activity

--- src/main/kotlin/spock/adb/command/GetActivityCommand.kt
+++ src/main/kotlin/spock/adb/command/GetActivityCommand.kt
@@ -8,14 +8,24 @@ import java.util.concurrent.TimeUnit
 class GetActivityCommand : Command<Any, String?> {
     override fun execute(p: Any, project: Project, device: IDevice): String? {
         val shellOutputReceiver = ShellOutputReceiver()
+        execShell(device, shellOutputReceiver, "dumpsys activity activities | grep mResumedActivity")
+        var outputStr = shellOutputReceiver.toString()
+        if(outputStr.isEmpty()){
+            execShell(device, shellOutputReceiver,"dumpsys activity activities | grep topResumedActivity")
+        }
+        outputStr = shellOutputReceiver.toString()
+        return outputStr.split(" ").find { it.contains("/") }
+            ?.replace("/.", ".")
+            ?.replace("}", "")
+            ?.replace(Regex(".+/"), "")
+    }
+
+    private fun execShell(device: IDevice, receiver: ShellOutputReceiver, command: String){
         device.executeShellCommand(
-            "dumpsys activity activities | grep mResumedActivity",
-            shellOutputReceiver,
+            command,
+            receiver,
             15L,
             TimeUnit.SECONDS
         )
-        return shellOutputReceiver.toString().split(" ").find { it.contains("/") }
-            ?.replace("/.", ".")
-            ?.replace(Regex(".+/"), "")
     }
 }
